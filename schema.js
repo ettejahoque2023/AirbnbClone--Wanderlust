8373d32773fbdf9f0cc7735e38f281ede87ac46e
Etteja Hoque <ettejahoque2003@gmail.com>
2025-01-08 12:01:00 +0530

    Frontend DOne here,All files are added.

--- schema.js
+++ schema.js
@@ -5,8 +5,8 @@ module.exports. listingSchema=Joi.object({
         title:Joi.string().required(),
         description:Joi.string().required(),
         location:Joi.string().required(),
-        country:Joi.string.required(),
-        price:Joi.number.required().min(0),
-        image:Joi.string.allow("",null),
+        country:Joi.string().required(),
+        price:Joi.number().required().min(0),
+        image:Joi.string().allow("",null),
         }).required()
 });
