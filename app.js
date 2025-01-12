a331f324b678ca3138caf3bae21d66e79111a759
Etteja Hoque <ettejahoque2003@gmail.com>
2025-01-12 22:11:09 +0530

    Finish styling the reviews and commnets part .And Add sessions with the project

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require ("express");
 const app= express();
 const mongoose = require('mongoose');
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate =require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError= require("./utils/ExpressError.js");
+const ExpressError=require("./utils/ExpressError.js");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-const {listingSchema}= require("./schema.js");
-const Review = require("./models/review.js");
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const listings= require("./routes/listing.js");
+const reviews= require("./routes/review.js");
 
 main()
 .then(()=>{
@@ -22,105 +23,41 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname,"/public")));
 
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+
+const sessionOptions ={
+  secret : "mySuperSecretCode",
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge:7 * 24 * 60 * 60 * 1000,
+    httpOnly:true,
+  },
+};
+
 app.get("/",(req,res)=>{
-    res.send("Hi, I am root");
+  res.send("Hi, I am root");
 });
 
+app.use(session(sessionOptions));
+app.use(flash());
 
-const validateListing=(req,res,next)=>{
-  let {error} = listingSchema.validate(req.body);
-  if(error)
-  {
-    throw new ExpressError(400,resourceLimits.error);
 
-  }
-  else{
-    next();
-  }
-}
+app.use((req,res,next) =>{
+  res.locals.success = req.flash("success");
+  res.locals.error=req.flash("error");
+  next();
+})
 
-//INDEX ROUT
-app.get("/listings", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-  }));
 
-//New Route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-  });
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
-//SHOW ROUTE
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-   let {id} = req.params;
-   const listing = await Listing.findById(id);
-   res.render("listings/show.ejs", { listing });
-}));
-
-//Create Route
-app.post("/listings", 
-  validateListing,
-  wrapAsync (async(req, res,next) => {
-    listingSchema.validate(req.body);
-    const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-}));
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-  }));
-  
-  //Update Route
-  app.put("/listings/:id", 
-    validateListing,
-    wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-  }));
-  
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-  }));
-//Reviews--Post route
-
-app.post("/listings/:id/reviews",async(req,res)=>{
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-
-  res.redirect(`/listings/${listing._id}`);
-});
-
-  
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing = new Listing({
-//         title:"My new villa",
-//         description : "by the beach",
-//         price:1200,
-//         location : "Goa",
-//         country:"India"
-//     });
-//     await sampleListing.save();
-//     console.log("Sample was saved");
-//     res.send("Successful testing");
-// });
 app.all("*",(req,res,next)=>{
   next(new ExpressError(404,"Page not found"));
 });
